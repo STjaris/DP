35f54f2e34d7191afaa5f8ece1e09708646c4f00
Soerano <tjariss@hotmail.nl>
2019-04-25 23:16:38 +0200

    Main update

--- P1/Main.java
+++ P1/Main.java
@@ -10,11 +10,11 @@ public class Main {
         System.out.println(r1.getNaam() + "  -  " + r1.getGbDatum());
         dao.save(r1);
 
-        Reiziger r2 = new Reiziger("Rick van Tol", "9/8/2000");
+        Reiziger r2 = new Reiziger("Jasper De Legend", "9/8/2000");
         System.out.println(r2.getNaam() + "  -  " + r2.getGbDatum());
         dao.save(r2);
 
-        Reiziger r3 = new Reiziger("Thom van Bemmelen", "19/5/2000");
+        Reiziger r3 = new Reiziger("Opa Wesley", "19/5/2000");
         System.out.println(r3.getNaam() + "  -  " + r3.getGbDatum());
         dao.save(r3);
 
